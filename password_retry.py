64d0b41468ee2c169f59b53551352a3bbac02d50
Sarah Wang <a0917460203@gmail.com>
2024-03-07 11:54:17 +0800

    change + to ,

--- password_retry.py
+++ password_retry.py
@@ -1,9 +1,10 @@
-chance = 3
+real_password = 'a123456'
+chance = 3 # 剩餘機會
 while chance > 0:
 	password = input('請輸入密碼: ')
-	if password == 'a123456':
+	if password == real_password:
 		print('登入成功!')
-		break
+		break   # 逃出迴圈
 	else:
 		chance = chance - 1
-		print('密碼錯誤!還有' + str(chance) + '次機會')
+		print('密碼錯誤!還有', chance, '次機會')
