ceb2f56ee448923513abc1ee92aff4531bec99af
Sarah Wang <a0917460203@gmail.com>
2024-03-07 12:04:11 +0800

    optimization of chance replay

--- password_retry.py
+++ password_retry.py
@@ -1,10 +1,14 @@
 real_password = 'a123456'
 chance = 3 # 剩餘機會
 while chance > 0:
+	chance = chance - 1
 	password = input('請輸入密碼: ')
 	if password == real_password:
 		print('登入成功!')
 		break   # 逃出迴圈
 	else:
-		chance = chance - 1
-		print('密碼錯誤!還有', chance, '次機會')
+		print('密碼錯誤!')
+		if chance > 0:
+			print('還有', chance, '次機會')
+		else:
+			print('沒機會嘗試了!要鎖帳號了啦!')
